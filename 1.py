e8499922c01423154a1d1513dc9acfb267e61234
AnothetLoice <anotherchild@163.com>
2016-06-30 16:40:49 +0800

    输入输出基础

--- 1.py
+++
@@ -1,5 +0,0 @@
-count=10**5
-nums=[]
-for i in range(count):
-	nums.insert(0,i)
-print("1")
---
+++ inputandoutput.py
@@ -0,0 +1,12 @@
+name=input("What is you name?\n")
+print(r"hello, "+name+u"!")
+x=int(input("x="))
+y=int(input("y="))
+print("x+y="+repr(x+y))
+print("x**y="+str(x**y))
+import cmath
+import math
+x=float(input("x="))
+print(str(cmath.sqrt(x)))
+print(str(math.ceil(x)))
+print(str(math.floor(x)))
